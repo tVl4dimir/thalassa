e88510c216b9bc34d84625f20b22ff806a5aa5e7
tVl4dimir <benpnz@hotmail.com>
2017-07-10 21:51:38 +0300

    Second branching push

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -100,7 +100,7 @@
                 </div>
                 
                 
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Δημιουργία</button>
                 
             </form>
         </div>
